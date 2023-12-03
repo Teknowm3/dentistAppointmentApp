09fc0368ead3549993ce166c6753a023826aea4a
olcay a <Hodri_meydan_izmir@hotmail.com>
2023-12-03 18:27:31 +0300

    V0.14.1

--- VeriYapılarıProjesi.py
+++ VeriYapılarıProjesi.py
@@ -1,4 +1,4 @@
-# Version 0.14
+# Version 0.14.1
 
 import tkinter as tk
 from tkinter import messagebox
@@ -70,29 +70,41 @@ class DentistAppointmentSystemUI:
 
         self.type_label = tk.Label(root, text="Randevu Türü:")
         self.type_var = tk.StringVar()
-        self.type_var.set("Lütfen Seçim Yapınız")  # Default olarak Diş Çekme seçili
-        self.type_menu = tk.OptionMenu(root, self.type_var, "Diş Muayne", "Temel Diş Bakımı", "Dolgu Muayenesi", "Diş Çekimi" , "Kanal Tedavisi" , "Diş Protezi" , "Ortodontik Muayne" , "Estetik Diş Hekimliği" , "Ağız Cerrahisi" , "Periodontal Tedavi" , "Pedodontik Randevu (Çocuk Diş Hekimliği)"
-        , "Temporomandibular Eklem (TMJ) Muayenesi")
-
+        self.type_var.set("Lütfen Seçim Yapınız")  # Default olarak Lütfen Seçim Yapınız kısmı seçili
+        self.type_menu = tk.OptionMenu(root, self.type_var, 
+                                       "Lütfen Secim Yapiniz", "Dis Muayne", "Temel Dis Bakimi", 
+                                       "Dolgu Muayenesi", "Dis Cekimi", "Kanal Tedavisi", "Dis Protezi", 
+                                       "Ortodontik Muayne", "Estetik Dis Hekimligi", "Agiz Cerrahisi", 
+                                       "Periodontal Tedavi", "Pedodontik Randevu (Cocuk Dis Hekimligi)", 
+                                       "Temporomandibular Eklem (TMJ) Muayenesi")
+        #   Randevu al buttonu
         self.schedule_button = tk.Button(root, text="Randevu Al", command=self.schedule_appointment)
-        self.cancel_button = tk.Button(root, text="Randevu Sil", command=self.cancel_appointment)
+        #   Randevuyu iptal et buttonu
+        self.cancel_button = tk.Button(root, text="Randevu Iptali", command=self.cancel_appointment)
+        #   Randevuları Göster buttonu
         self.display_button = tk.Button(root, text="Randevu Listesini Görüntüle", command=self.display_appointments)
 
         # Arayüz elemanlarını grid'e yerleştir
-        self.name_label.grid(row=0, column=0, padx=40, pady=10)
+        #   1. input
+        self.name_label.grid(row=0, column=0, padx=10, pady=10)
         self.name_entry.grid(row=0, column=1, padx=10, pady=10)
-
+        #   2. input
         self.date_label.grid(row=1, column=0, padx=10, pady=10)
         self.date_entry.grid(row=1, column=1, padx=10, pady=10)
-
+        #   3. input
         self.time_label.grid(row=2, column=0, padx=10, pady=10)
         self.time_entry.grid(row=2, column=1, padx=10, pady=10)
-
+        #   4. Seçim Menüsü
         self.type_label.grid(row=3, column=0, padx=10, pady=10)
         self.type_menu.grid(row=3, column=1, padx=10, pady=10)
-
-        self.schedule_button.grid(row=4, column=0, pady=10)
-        self.cancel_button.grid(row=5, column=0, pady=10)
+        
+        #   Button'ların yerleştirmeleri
+        
+        #     Randevu al'ın 
+        self.schedule_button.grid(row=4, column=0,  columnspan=2, padx=10, pady=10)
+        #     Randevu'yu Sil'in   
+        self.cancel_button.grid(row=5, column=0,  columnspan=2, padx=10, pady=10)
+        #     Randevuları Listele'nin
         self.display_button.grid(row=6, column=0, columnspan=2, pady=10, padx=80)
 
     def schedule_appointment(self):
@@ -103,16 +115,20 @@ class DentistAppointmentSystemUI:
         appointment_type = self.type_var.get()
 
         try:
-            #   Tarih ve saat girişlerini doğru formatta kontrol et
+            #   Tarih ve saat girişlerini doğru formatta mı olduğunu kontrol et.
             appointment_datetime = datetime.strptime(f"{date} {time}", "%Y-%m-%d %H:%M")
+            #   ValueError Çıktısını değiştiriyoz. 
         except ValueError:
+            #   Hata Pencersesinde Tarih veya zamanın hatalı olduğuna dair bir hata mesajı yolla. 
             messagebox.showerror("Hata", "Lütfen geçerli bir tarih ve saat girin (örneğin, '2023-11-26 14:30').")
             return
 
         #   Randevu almak için gerekli kontrolleri yap
+        #   Default Seçimdeyse Hata Ver.
         if appointment_type == "Lütfen Seçim Yapınız":
             messagebox.showerror("Hata", "Lütfen geçerli bir randevu türü seçin.")
             return
+        #   Randevu Oluştu.
         elif self.dentist_system.schedule_appointment(patient_name, date, time, appointment_type):
             messagebox.showinfo("Başarılı", "Randevu başarıyla alındı.")
         else:
@@ -122,17 +138,19 @@ class DentistAppointmentSystemUI:
         #   Girilen Bilgileri Al.
         date = self.date_entry.get()
         time = self.time_entry.get()
-
+        #   Varsa Randevuyu iptal if Et / else Etme.
         if self.dentist_system.cancel_appointment(date, time):
             messagebox.showinfo("Başarılı", "Randevu başarıyla iptal edildi.")
         else:
-            messagebox.showerror("Hata", "Randevu iptal edilemedi. Belirtilen tarihte ve saatte randevu bulunamadı.")
+            messagebox.showerror("Hata", "Randevu iptal edilemedi. \nBelirtilen tarihte ve saatte randevu bulunamadı.")
 
     def display_appointments(self):
         #   Girilen Bilgileri Al.
         appointments = self.dentist_system.get_appointments()
+        #   Randevu Kontrolü
         if not appointments:
             messagebox.showinfo("Bilgi", "Henüz randevu bulunmamaktadır.")
+        #   Randevuyu Göster
         else:
             appointment_str = "\n".join([f"Hasta : {appointment['patient_name']} | Muayne Tarihi : {appointment['date']} | Saat : {appointment['time']} | Muayne Türü : {appointment['type']}" for appointment in appointments])
             messagebox.showinfo("Randevu Listesi", appointment_str)
